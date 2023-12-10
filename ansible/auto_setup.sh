9e6fb4c548f872b1b585981048a0443be9f07a13
Connor Voisey <connor.voisey@gmail.com>
2023-12-10 15:53:31 +0000

    feat: added auto setup script

---
+++ ansible/auto_setup.sh
@@ -0,0 +1,8 @@
+sudo apt install git software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+
+git clone https://github.com/ConnorVoisey/configurations.git $HOME/configurations
+
+cd $HOME/configurations/ansible
+ansible-playbook main.yml -i inventory -K
